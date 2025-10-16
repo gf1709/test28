4cc3e61c9e01bb18215aa4371bab970e8aaaf98a
fc0382 <gregorio.fracella@allitude.it>
2025-10-16 12:30:12 +0200

    env varibles

--- client-react/src/components/EmployeeList.jsx
+++ client-react/src/components/EmployeeList.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+
+
+const URL = import.meta.env.VITE_BASE_URL + "/api/employees";
+
 function EmployeeList() {
+  const [temp, setTemp] = useState();
+
+  useEffect(() => {
+    console.log("URL is=", URL);
+    const fetchData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      setTemp(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
+    <div className="container d-flex justify-content-center text-center">
       <h2>Employee List</h2>
     </div>
   );
--- server/src/main/java/com/test28/controller/WebController.java
+++ server/src/main/java/com/test28/controller/WebController.java
@@ -3,6 +3,7 @@ package com.test28.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.test28.repo.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*") // Commenting out as it already allows all origins
 public class WebController {
 
     private final EmployeeRepository employeeRepository;
